bc19bac4e4ea970b77e1e96d9466dd22d78d741b
bisayaDev <bagsprin@gmail.com>
2024-07-04 17:02:46 +0800

    update ministry migration

--- database/migrations/2024_07_01_103624_create_ministries_table.php
+++ database/migrations/2024_07_01_103624_create_ministries_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('ministries', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedInteger('head_id');
+            $table->unsignedInteger('assistant_id');
             $table->timestamps();
         });
     }
