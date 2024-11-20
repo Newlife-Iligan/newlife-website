d59db093267b5431b0d01b2998a148ad137b1082
bisayaDev <bagsprin@gmail.com>
2024-11-20 21:16:37 +0800

    add zapier

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
 
+Route::post('/zapier/enable', function (){
+    return response()->json(['status' => 200]);
+});
+Route::post('/zapier/disable', function (){
+    return response()->json(['status' => 200]);
+});
